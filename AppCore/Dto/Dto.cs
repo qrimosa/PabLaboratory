513106a5c501942f2ee92c716849ed95a8e2c5fc
qrimosa <maratiskhakov9@gmail.com>
2026-04-15 10:36:40 +0200

    Lab07 initial

--- AppCore/Dto/Dto.cs
+++ AppCore/Dto/Dto.cs
@@ -114,3 +114,24 @@ public class NoteDto
     public DateTime CreatedAt { get; set; }
     public string? CreatedBy { get; set; } 
 }
+
+//--- Login ---
+public record LoginDto
+{
+    public string Email { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
+	
+public record AuthResponseDto
+{
+    public string AccessToken { get; init; } = string.Empty;
+    public string RefreshToken { get; init; } = string.Empty;
+    public DateTime ExpiresAt { get; init; }
+    public UserDto User { get; init; } = null!;
+}
+	
+	
+public record RefreshTokenDto(
+    string AccessToken,
+    string RefreshToken
+);
